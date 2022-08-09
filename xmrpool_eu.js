d2d330f7e5a8d9e928a04688a5bda53247296f8e
regenschein71 <regenschein71@users.noreply.github.com>
2022-08-09 14:54:32 +0200

    Create xmrpool_eu.js
    First working version

---
+++ xmrpool_eu.js
@@ -0,0 +1,193 @@
+//
+// xmrpool.eu widget
+//
+// Displays your current balance, hashrate and top workers 
+// Add your wallet address, that you are using in xmrig, as parameter
+
+
+// Expected input parameters: |-separated list of strings
+// walletAddress (only parameter at the moment)
+
+// Number of workers to display
+const maxWorkers = 3
+
+// Length of the balance string
+const balanceLength = 7
+
+const statsFontSize = 16
+
+const workerFontSize = 13
+
+// Check the input parameter
+const expectedInput="walletAddress"
+let walletAddress
+let widgetInput = args.widgetParameter
+
+if (widgetInput !== null) {
+  [walletAddress] = widgetInput.split("|");
+
+  if (!walletAddress) {
+    throw new Error("Invalid parameter. Expected format: " + expectedInput)
+  }
+
+} else {
+  throw new Error("No Widget paramter set. Expected format: " + expectedInput)
+}
+
+// Mining stats URL
+const apiURL = (wallet) => `https://web.xmrpool.eu:8119/stats_address?address=${wallet}&longpoll=false`
+
+let data = await loadData(walletAddress)
+let widget = await createWidget(data)
+
+if (!config.runsInWidget) {
+  await widget.presentSmall()
+}
+
+log(widget)
+
+Script.setWidget(widget)
+Script.complete()
+
+async function loadData(wallet) {
+  
+  // log("Load data from " + apiURL(walletAddress))
+  
+  const data = await new Request(apiURL(walletAddress)).loadJSON()
+
+  if (data.stats.length === 0) {
+    return { error: 1 }
+  }
+  
+  return data
+}
+
+// Format the balance value. Balance is a long value in the JSON, must be divided by 10^12 to 
+// get the actual XMR value
+function formatBalance(value, length) {
+  if (!value) {
+    return '-'
+  }
+
+  let price = (parseFloat(value) / 1000000000000).toString().slice(0, length)
+  return price
+}
+
+// Convert a hashrate of format 1.23 KH to 1023
+function calcHashrate(hStr) {
+  
+  if (hStr == null) {
+    return 0
+  }
+  
+  [a, unit] = hStr.split(" ")
+  let val = parseFloat(a)
+  
+  switch(unit) {
+  case "H":
+    break
+  case "KH": 
+    val = val * 1000
+    break
+  case "MH": 
+    val = val * 1000000
+    break
+  case "GH": 
+    val = val * 1000000000
+    break
+  case "TH":
+    // Terahash... I wish... 
+    val = val * 1000000000000
+    break
+  }
+  // log(hStr + " -> " + val)
+  
+  return val
+}
+
+function hashrate(worker) {
+  if (worker.hashrate == null) {
+    return "0 H"
+  }
+  return worker.hashrate
+}
+function sortWorkerByHashrate(a,b) {
+  // log("Comparing " + a.hashrate + b)
+  return calcHashrate(hashrate(b)) - calcHashrate(hashrate(a))
+}
+
+// Add a single worker with its hashrate to the list
+function addWorker(list, worker) {
+  
+    list.addSpacer(1)
+    let wStack = list.addStack()
+    wStack.addSpacer(5)
+    wStack.addText(worker.workerId.slice(0,15)).font = Font.lightSystemFont(workerFontSize)
+    wStack.addSpacer()
+    wStack.addText(hashrate(worker)).font = Font.lightSystemFont(workerFontSize)
+
+}
+
+async function createWidget(data) {
+
+  const list = new ListWidget()
+  list.setPadding(0, 2, 2, 2)
+
+  if (data.error) {
+    log("data error")
+    let errorMessage = list.addText('No stats found')
+    errorMessage.font = Font.boldSystemFont(12)
+    errorMessage.textColor = textColor
+    errorMessage.centerAlignText()
+    
+    return list
+  }
+
+  const stats = data.stats
+  const workers = data.perWorkerStats.sort(sortWorkerByHashrate)
+    
+  // Header line
+  let headerText = list.addText("xmrpool.eu")
+  headerText.font = Font.headline()
+  headerText.centerAlignText()
+  
+  list.addSpacer(3)
+  
+  // Balance line
+  let lineStack = list.addStack()
+  lineStack.addText("Balance:").font = Font.mediumSystemFont(statsFontSize)
+  lineStack.addSpacer()
+  lineStack.addText(formatBalance(stats.balance, balanceLength)).font = Font.mediumSystemFont(statsFontSize)
+  
+  list.addSpacer(1)
+  
+  // Hashrate line
+  let rateStack = list.addStack()
+  rateStack.addText("Rate:").font = Font.mediumSystemFont(statsFontSize)
+  rateStack.addSpacer()
+  let rateText = rateStack.addText(stats.hashrate).font = Font.mediumSystemFont(statsFontSize)
+
+  list.addSpacer(1)
+
+  // # Workers line
+  let workerStack = list.addStack()
+  workerStack.addText("Workers:").font = Font.mediumSystemFont(statsFontSize)
+  workerStack.addSpacer()
+  let workerText = workerStack.addText(workers.length.toString()).font = Font.mediumSystemFont(statsFontSize)
+
+
+  
+  // log(workers)   
+  
+  // Add the hashrates for the fastest workers
+  for (let i=0; i < Math.min(workers.length, maxWorkers); i++) {
+     addWorker(list, workers[i])
+  }
+  
+  // If there are more, unlisted workers, add a "..."
+  if (workers.length > maxWorkers) {
+    list.addText("...").centerAlignText()
+  }
+  
+  return list
+}
